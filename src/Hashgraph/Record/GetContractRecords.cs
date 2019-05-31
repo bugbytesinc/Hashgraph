f15a93d67b50f7200c70e236bce3db657151a52a
Jason Fabritz <msdn@bugbytes.com>
2019-05-31 08:33:54 -0400

    Fix #36 Get Transaction Records for Contract

---
+++ src/Hashgraph/Record/GetContractRecords.cs
@@ -0,0 +1,68 @@
+﻿using Grpc.Core;
+using Hashgraph.Implementation;
+using Proto;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hashgraph
+{
+    public partial class Client
+    {
+        /// <summary>
+        /// Retrieves the records associated with an contract that are presently
+        /// held within the network.
+        /// </summary>
+        /// <param name="contract">
+        /// The Hedera Network Contract Address to retrieve associated records.
+        /// </param>
+        /// <param name="configure">
+        /// Optional callback method providing an opportunity to modify 
+        /// the execution configuration for just this method call. 
+        /// It is executed prior to submitting the request to the network.
+        /// </param>
+        /// <returns>
+        /// A detailed description of the account.
+        /// </returns>
+        /// <exception cref="ArgumentOutOfRangeException">If required arguments are missing.</exception>
+        /// <exception cref="InvalidOperationException">If required context configuration is missing.</exception>
+        /// <exception cref="PrecheckException">If the gateway node create rejected the request upon submission.</exception>
+        public async Task<TransactionRecord[]> GetContractRecordsAsync(Address contract, Action<IContext>? configure = null)
+        {
+            contract = RequireInputParameter.Contract(contract);
+            var context = CreateChildContext(configure);
+            var gateway = RequireInContext.Gateway(context);
+            var payer = RequireInContext.Payer(context);
+            var transfers = Transactions.CreateCryptoTransferList((payer, -context.FeeLimit), (gateway, context.FeeLimit));
+            var transactionId = Transactions.GetOrCreateTransactionID(context);
+            var transactionBody = Transactions.CreateCryptoTransferTransactionBody(context, transfers, transactionId, "Get Contract Records");
+            var query = new Query
+            {
+                ContractGetRecords = new ContractGetRecordsQuery
+                {
+                    Header = Transactions.SignQueryHeader(transactionBody, payer),
+                    ContractID = Protobuf.ToContractID(contract)
+                }
+            };
+            var response = await Transactions.ExecuteRequestWithRetryAsync(context, query, getRequestMethod, getResponseCode);
+            ValidateResult.PreCheck(transactionId, getResponseCode(response));
+            return response.ContractGetRecordsResponse.Records.Select(record =>
+            {
+                var result = new TransactionRecord();
+                Protobuf.FillRecordProperties(record, result);
+                return result;
+            }).ToArray();
+
+            static Func<Query, Task<Response>> getRequestMethod(Channel channel)
+            {
+                var client = new SmartContractService.SmartContractServiceClient(channel);
+                return async (Query query) => (await client.getTxRecordByContractIDAsync(query));
+            }
+
+            static ResponseCodeEnum getResponseCode(Response response)
+            {
+                return response.ContractGetRecordsResponse?.Header?.NodeTransactionPrecheckCode ?? ResponseCodeEnum.Unknown;
+            }
+        }
+    }
+}
---
+++ test/Hashgraph.Test/Record/GetContractRecordTests.cs
@@ -0,0 +1,69 @@
+﻿using Hashgraph.Test.Fixtures;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Hashgraph.Test.Record
+{
+    [Collection(nameof(NetworkCredentialsFixture))]
+    public class GetContractRecordTests
+    {
+        private readonly NetworkCredentialsFixture _networkCredentials;
+        public GetContractRecordTests(NetworkCredentialsFixture networkCredentials, ITestOutputHelper output)
+        {
+            _networkCredentials = networkCredentials;
+            _networkCredentials.TestOutput = output;
+        }
+        [Fact(DisplayName = "Contract Records: Contract with no records returns no records.")]
+        public async Task ContractWithNoRecords()
+        {
+            await using var fx = await StatefulContractInstance.CreateAsync(_networkCredentials);
+
+            var transactionCount = Generator.Integer(2, 5);
+            for (int i = 0; i < transactionCount; i++)
+            {
+                await fx.Client.CallContractWithRecordAsync(new CallContractParams
+                {
+                    Contract = fx.ContractCreateRecord.Contract,
+                    Gas = 30_000,
+                    FunctionName = "get_message"
+                });
+            }
+            var records = await fx.Client.GetContractRecordsAsync(fx.ContractCreateRecord.Contract);
+            Assert.NotNull(records);
+            Assert.Empty(records);
+        }
+        [Fact(DisplayName = "Contract Records: Casting Account Address as Contract returns no records (BUT DOES NOT FAIL)")]
+        public async Task CanGetTransactionRecordsForAccount()
+        {
+            await using var fx = await TestAccountInstance.CreateAsync(_networkCredentials);
+
+            var transactionCount = Generator.Integer(2, 5);
+            var childAccount = new Account(fx.AccountRecord.Address, fx.PrivateKey);
+            var parentAccount = _networkCredentials.CreateDefaultAccount();
+            await fx.Client.TransferAsync(parentAccount, childAccount, transactionCount * 100001);
+            await using (var client = fx.Client.Clone(ctx => ctx.Payer = childAccount))
+            {
+                for (int i = 0; i < transactionCount; i++)
+                {
+                    await client.TransferAsync(childAccount, parentAccount, 1);
+                }
+            }
+            var records = await fx.Client.GetContractRecordsAsync(childAccount);
+            Assert.NotNull(records);
+            Assert.Empty(records);
+        }
+        [Fact(DisplayName = "Contract Records: Get with Empty Contract address raises Error.")]
+        public async Task EmptyAccountRaisesError()
+        {
+            await using var client = _networkCredentials.CreateClientWithDefaultConfiguration();
+            var ane = await Assert.ThrowsAsync<ArgumentNullException>(async () =>
+            {
+                await client.GetContractRecordsAsync(null);
+            });
+            Assert.Equal("contract", ane.ParamName);
+            Assert.StartsWith("Contract Address is missing. Please check that it is not null.", ane.Message);
+        }
+    }
+}
