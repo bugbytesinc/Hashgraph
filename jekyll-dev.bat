2051a5ff4f1b296fb3ad3f496e4fc80961ab39a0
Jason Fabritz <msdn@bugbytes.com>
2019-07-10 18:08:04 -0400

    Getting paths working in dev/prod docs.

--- jekyll-dev.bat
+++ jekyll-dev.bat
@@ -1,4 +1,4 @@
 @echo off
 if not exist "%cd%/.jekyll-cache" mkdir "%cd%/.jekyll-cache
-docker run --rm -it --volume="%cd%/docs:/srv/jekyll" --volume="%cd%/.jekyll-cache:/usr/local/bundle" -p 4000:4000 -p 35729:35729 jekyll/jekyll:3.8  jekyll serve --force_polling --livereload
+docker run --rm -it --volume="%cd%/docs:/srv/jekyll" --volume="%cd%/.jekyll-cache:/usr/local/bundle" -p 4000:4000 -p 35729:35729 -e "JEKYLL_ENV=docker" jekyll/jekyll:3.8  jekyll serve --force_polling --livereload --config  _config.yml,_config.docker.yml
 rem docker run --rm -it --volume="%cd%/docs:/srv/jekyll" --volume="%cd%/.jekyll-cache:/usr/local/bundle" -p 4000:4000 jekyll/jekyll:3.8  bash
